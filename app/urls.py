a84a8a5119aa6850156731c86b68c8c43e5a4f0d
Emmanuel <nwekeemmanuel667@gmail.com>
2025-08-17 19:17:11 +0100

    Update services template and urls

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
-from django.conf import settings
-from django.conf.urls.static import static
+
 urlpatterns = [
     path('', views.home,name='home'),
     path('about', views.about,name='about'),
@@ -16,5 +15,3 @@ urlpatterns = [
     path('add_post', views.add_post,name='add_post'),
     path('single/<int:pk>', views.single,name='single')
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
